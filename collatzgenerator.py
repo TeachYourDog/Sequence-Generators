abf0dfddaf293c1ad539998ab91d3700bd4c022b
Matt Breen <mcjbreen@gmail.com>
2021-04-16 04:24:33 -0400

    Update collatzgenerator.py

--- collatzgenerator.py
+++ collatzgenerator.py
@@ -1,17 +1,22 @@
 
-
-
-
-
-
-def collatz(a):
+def collatz(a,numlist):
+    
     while a != 1:
         if a%2 == 0:
             a = a / 2
             print(a)
+            numlist.append(a)
         else:
             a = 3*a + 1
             print(a)
+            numlist.append(a)
 
 
-collatz(2)
+a = input("What is the first number?")
+a = int(a)
+
+numlist = [a]
+
+print(a)
+collatz(a,numlist)
+print(len(numlist))
